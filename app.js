91efba4c8d640af41e5eb0398528d74952db41bd
Binjunaid06 <ayanbinjunaid@gmail.com>
2024-12-25 22:50:29 +0500

    added app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+//question 1 chapter 6-9
+
+let a = 10;
+
+document.write(`Result: <br>`)
+
+document.write(`The value of a is ${a++}`);
+
+document.write("<br>");
+
+document.write("-------------------------------------------------------------");
+
+document.write("<br>");
+
+document.write(`The value of a++ is now ${a}`)
+
+document.write("<br>");
+
+//the 11 value one
+
+a=11
+
+
+
+document.write(`The value of a++ is ${a++}`);
+
+document.write("<br>");
+
+document.write("-------------------------------------------------------------");
+
+document.write("<br>");
+
+document.write(`Result: <br>`)
+
+document.write(`The value of a++ is now ${a}`)
